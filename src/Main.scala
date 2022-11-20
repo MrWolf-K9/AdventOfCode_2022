6dedf02da4143373ab140bc34e7677e5fda7cf3f
Patrik Michal Vlček <vlcekpatrikmichal@gmail.com>
2022-11-20 21:29:47 +0100

    feat: implemented part one

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,31 @@
+import scala.io.BufferedSource
+
 object Main {
+  val source: BufferedSource = scala.io.Source.fromFile("day9_input1.txt")
+  val lines: Array[String] = try source.mkString.split("\n") finally source.close()
+  //val lines: Array[String] = Array("2199943210", "3987894921", "9856789892", "8767896789", "9899965678")
+  val maxX: Int = lines(0).length
+  val maxY: Int = lines.length
+
+  def getPoint(x: Int, y: Int): Int = {
+    if (x < 0 || x >= maxX) || (y < 0 || y >= maxY) then return 10
+    lines(y)(x).asDigit
+  }
+
+  def isLowPoint(x: Int, y: Int, value: Int): Boolean = {
+    value < getPoint(x + 1, y) && value < getPoint(x, y + 1) && value < getPoint(x - 1, y) && value < getPoint(x, y - 1)
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println(
+      lines.zipWithIndex.map(
+        (line, indexY) => line.zipWithIndex.flatMap(
+          (char, indexX) =>
+            if isLowPoint(indexX, indexY, char.toString.toInt)
+            then Iterable(getPoint(indexX, indexY) + 1)
+            else Iterable(0)
+        ).sum
+      ).sum
+    )
   }
 }
