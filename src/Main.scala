c959effda289b80aaf16bf895e9ac3b44d4be0be
Patrik Michal Vlček <vlcekpatrikmichal@gmail.com>
2022-11-21 13:19:40 +0100

    feat: implemented day 10 and tested on example

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,4 @@
-import days_2021.Day_09
+import days_2021.*
 
 import scala.annotation.unused
 import scala.io.BufferedSource
@@ -7,6 +7,6 @@ object Main {
 
 
   def main(args: Array[String]): Unit = {
-    Day_09.run()
+    Day_10.run()
   }
 }
---
+++ src/days_2021/Day_10.scala
@@ -0,0 +1,56 @@
+package days_2021
+
+import scala.annotation.tailrec
+
+object Day_10 {
+  val lines: Array[String] = Array(
+    "[({(<(())[]>[[{[]{<()<>>",
+    "[(()[<>])]({[<{<<[]>>(",
+    "{([(<{}[<>[]}>{[]{[(<()>",
+    "(((({<>}<{<{<>}{[]{[]{}",
+    "[[<[([]))<([[{}[[()]]]",
+    "[{[{({}]{}}([{[{{{}}([]",
+    "{<[[]]>}<{[{[{[]{()[[[]",
+    "[<(<(<(<{}))><([]([]()",
+    "<{([([[(<>()){}]>(<<{{",
+    "<{([{{}}[<[[[<>{}]]]>[]]")
+
+  def handleChar(stack: Array[Char], chr: Char): Either[Array[Char], Int] = {
+    var stackCopy = stack
+    chr match
+      case chr if chr == '(' || chr == '[' || chr == '{' || chr == '<' => stackCopy = stack :+ chr
+      case chr => if isClosing(stackCopy.last, chr) then stackCopy = stackCopy.init else return Right(getScore(chr))
+    Left(stackCopy)
+  }
+
+  def getScore(chr: Char): Int = {
+    chr match
+      case ')' => 3
+      case ']' => 57
+      case '}' => 1197
+      case '>' => 25137
+  }
+
+  def isClosing(start: Char, close: Char): Boolean = {
+    (start, close) match
+      case ('(', ')') => true
+      case ('[', ']') => true
+      case ('{', '}') => true
+      case ('<', '>') => true
+      case (_, _) => false
+  }
+
+  @tailrec
+  def handleLine(array: Array[Char], stack: Array[Char], index: Int): Int = {
+    if index >= array.length then return 0
+    val result = handleChar(stack, array(index))
+    result match
+      case Left(value) => handleLine(array, value, index + 1)
+      case Right(value) => value
+  }
+
+  def run(): Unit = {
+    println(lines.map(line => handleLine(line.toCharArray, Array(), 0)).filter(x => x != 0).sum)
+  }
+
+}
